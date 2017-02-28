721fd0dba9b59aa1ba331ca6200501c59c07354a
S@ndez <vimmrana0@gmail.com>
2017-02-28 14:49:43 +0545

    setting debug to false

--- blog/settings.py
+++ blog/settings.py
@@ -23,7 +23,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = 'm@g)(fbwdh5wc*xe@j++m9rh^uza5se9a57c5ptwkg*b@ki0x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['vimm0-blog.herokuapp.com']
 
