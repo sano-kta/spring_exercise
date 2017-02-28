e2f5bb68c14026df6dae1e2937581a70edee7f8a
S@ndez <vimmrana0@gmail.com>
2017-02-28 13:24:11 +0545

    commenting on the edit part

--- Advancing-the-Blog/src/blog/wsgi.py
+++ Advancing-the-Blog/src/blog/wsgi.py
@@ -10,11 +10,13 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+#DjangoWhiteNoise
 from whitenoise.django import DjangoWhiteNoise
 
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blog.settings")
 
 application = get_wsgi_application()
+#DjangoWhiteNoise
 application = DjangoWhiteNoise(application)
 
